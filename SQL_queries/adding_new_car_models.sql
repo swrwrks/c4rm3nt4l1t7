533fad6c89c4d31a647b6f73215042e82af7434d
Er4-ball <cabanoffegor@yandex.ru>
2026-04-19 21:07:39 +0300

    Add files via upload

---
+++ SQL_queries/adding_new_car_models.sql
@@ -0,0 +1,8 @@
+INSERT INTO cars (brand_id, model, year, price, color, mileage, status) VALUES
+(1, 'Outback', 2023, 3200000.00, 'Silver', 12000, 'available'),
+(1, 'Forester', 2022, 2900000.00, 'Blue', 18000, 'available');
+
+-- Hyundai (brand_id = 9)
+INSERT INTO cars (brand_id, model, year, price, color, mileage, status) VALUES
+(2, 'Santa Fe', 2023, 3500000.00, 'Black', 8000, 'available'),
+(2, 'Tucson', 2022, 2700000.00, 'White', 15000, 'available');
