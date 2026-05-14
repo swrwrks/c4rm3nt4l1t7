52e883c61bece6bbf256e004aff3abbbbe13d757
swrwrks <swrwrks@mail.ru>
2026-05-14 17:33:30 +0300

    zxc

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import cars, brands, auth
 from src.db_connector import db
 
@@ -9,6 +10,17 @@ async def lifespan(app: FastAPI):
     db.close()
 
 app = FastAPI(title="Cars API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cars.router)
 app.include_router(brands.router)
