8fff423cda8982c12afc2e984fb8feba9e00004a
swrwrks <swrwrks@mail.ru>
2026-05-21 19:51:15 +0300

    front for u+adminpanel

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from src.routers import cars, brands, auth
+from fastapi.middleware.cors import CORSMiddleware
+from src.routers import cars, brands, auth, users
 from src.db_connector import db
 
 @asynccontextmanager
@@ -10,9 +11,21 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Cars API", lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(cars.router)
 app.include_router(brands.router)
 app.include_router(auth.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
