3e4ee94f9c5f305e0f5b476ad8f02a3bae49b27a
swrwrks <swrwrks@mail.ru>
2026-05-25 22:39:57 +0300

    Backend maybe finished v2

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routers import cars, brands, auth, users
+from src.routers import cars, brands, auth, users, about
 from src.db_connector import db
 
 @asynccontextmanager
@@ -26,6 +26,7 @@ app.include_router(cars.router)
 app.include_router(brands.router)
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(about.router)
 
 @app.get("/")
 def root():
